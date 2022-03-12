cd0c383d8fa7c6f0585e8281f9e7c79bb8261915
Thien Dang <ctap105@gmail.com>
2022-03-12 07:05:12 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { getAllCharacters } from './store/character'
 
+import AppNavigation from './components/AppNavigation';
+import { AddOperation } from 'three';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -21,12 +23,12 @@ function App() {
 
   return (
     <BrowserRouter>
-
-      <Switch>
-        <Route path='/' exact={true} >
-          <h1>My Home Page</h1>
-        </Route>
-      </Switch>
+      <AppNavigation />
+        <Switch>
+          <Route path='/' exact={true} >
+            <h1>My Home Page</h1>
+          </Route>
+        </Switch>
     </BrowserRouter>
   );
 }
