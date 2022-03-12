a46690a4a550258cd86cde281dc1f265447d0174
Thien Dang <ctap105@gmail.com>
2022-03-12 06:32:10 -0500

    fetched all characters

--- src/store/character.js
+++ src/store/character.js
@@ -9,6 +9,29 @@ export const allCharacters = payload => {
 
 export const getAllCharacters = () => async dispatch => {
     console.log('henlo world')
+    async function fetchCharacters(page, characters) {
+        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
+        const data = await response.json()
+
+        const totalPages = Number(data.info.pages)
+
+        characters = [...characters, ...data.results]
+
+        if (page < totalPages) {
+            return await fetchCharacters(page + 1, characters)
+        } else {
+            return characters
+        }
+    }
+
+    const characterList = () => {
+        return fetchCharacters(1, [])
+    }
+
+    characterList().then(data => {
+        dispatch(allCharacters((data)))
+        return data
+    })
 }
 
 export const initialState = {entries:[]}
